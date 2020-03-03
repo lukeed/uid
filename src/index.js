d0f2c372da0f5981db7fe60c4322a4e7808872d0
Luke Edwards <luke.edwards05@gmail.com>
2020-03-02 23:05:34 -0800

    feat: module logic

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var IDX=36, HEX='';
+while (IDX--) HEX += IDX.toString(36);
+
+export default function (len) {
+	var str='', num = len || 8;
+	while (num--) str += HEX[Math.random() * 36 | 0];
+	return str;
+}
