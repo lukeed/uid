c01fa24d3fd4625b17da38d1a0dfe6318367b9bf
Luke Edwards <luke.edwards05@gmail.com>
2020-03-02 23:09:05 -0800

    chore: add collision runner

---
+++ test/collisions/index.js
@@ -0,0 +1,23 @@
+const foid = require('../../dist');
+
+// $ node test/collisions 16 1e7
+const [len=8, cycles] = process.argv.slice(2);
+const total = cycles ? +cycles : 1e6;
+
+console.log('~> item total:', total.toLocaleString());
+console.log('~> hash length:', len);
+
+let sentry = new Set();
+let i=0, tmp, duplicates=0;
+for (; i < total; i++) {
+	tmp = foid(len);
+	if (sentry.has(tmp)) {
+		duplicates++;
+	} else {
+		sentry.add(tmp);
+	}
+}
+
+console.log('iterations:', total.toLocaleString());
+console.log('collisions:', duplicates.toLocaleString());
+console.log('percentage:', (duplicates / total * 100).toFixed(4) + '%');
