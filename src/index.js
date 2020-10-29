7f28d08a58e1afe7ce299a583f66144fb3e67e9d
Luke Edwards <luke.edwards05@gmail.com>
2020-10-29 08:41:38 -0700

    fix(default): maximize buffer cache

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ while (IDX--) HEX[IDX] = (IDX + 256).toString(16).substring(1);
 
 export function uid(len) {
 	var i=0, tmp=(len || 11);
-	if (!BUFFER || ((IDX + tmp) > SIZE)) {
+	if (!BUFFER || ((IDX + tmp) > SIZE*2)) {
 		for (BUFFER='',IDX=0; i < SIZE; i++) {
 			BUFFER += HEX[Math.random() * 256 | 0];
 		}
